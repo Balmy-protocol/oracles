e856ecfda161e74032595cee06fff6d8182bc4bb
Sam Bugs <101145325+0xsambugs@users.noreply.github.com>
2022-06-11 22:52:53 -0300

    feat: add multicall (#13)

--- test/e2e/oracle-aggregator.spec.ts
+++ test/e2e/oracle-aggregator.spec.ts
@@ -5,12 +5,14 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { OracleAggregatorMock, OracleAggregatorMock__factory, IPriceOracle } from '@typechained';
 import { snapshot } from '@utils/evm';
 import { smock, FakeContract } from '@defi-wonderland/smock';
+import { BigNumber } from 'ethers';
 
 chai.use(smock.matchers);
 
 describe('OracleAggregator', () => {
   const TOKEN_A = '0x0000000000000000000000000000000000000001';
   const TOKEN_B = '0x0000000000000000000000000000000000000002';
+  const TOKEN_C = '0x0000000000000000000000000000000000000003';
   let superAdmin: SignerWithAddress, admin: SignerWithAddress;
   let oracleAggregator: OracleAggregatorMock;
   let superAdminRole: string, adminRole: string;
@@ -50,6 +52,30 @@ describe('OracleAggregator', () => {
           expect(oracle).to.equal(oracle1.address);
           expect(forced).to.be.false;
         });
+      });
+    });
+  });
+
+  describe('multicall', () => {
+    const QUOTE_ORACLE_1 = BigNumber.from(10);
+    const QUOTE_ORACLE_2 = BigNumber.from(20);
+    when('executing multiple quotes', () => {
+      let result1: string, result2: string;
+      given(async () => {
+        oracle1['quote(address,uint256,address,bytes)'].returns(QUOTE_ORACLE_1);
+        oracle2['quote(address,uint256,address,bytes)'].returns(QUOTE_ORACLE_2);
+        await oracleAggregator.connect(admin).forceOracle(TOKEN_A, TOKEN_B, oracle1.address);
+        await oracleAggregator.connect(admin).forceOracle(TOKEN_A, TOKEN_C, oracle2.address);
+
+        const { data: quote1Data } = await oracleAggregator.populateTransaction['quote(address,uint256,address)'](TOKEN_A, 1, TOKEN_B);
+        const { data: quote2Data } = await oracleAggregator.populateTransaction['quote(address,uint256,address)'](TOKEN_A, 1, TOKEN_C);
+        [result1, result2] = await oracleAggregator.callStatic.multicall([quote1Data!, quote2Data!]);
+      });
+      then('first quote was returned correctly', async () => {
+        expect(BigNumber.from(result1)).to.equal(QUOTE_ORACLE_1);
+      });
+      then('second quote was returned correctly', async () => {
+        expect(BigNumber.from(result2)).to.equal(QUOTE_ORACLE_2);
       });
     });
   });
