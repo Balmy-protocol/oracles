b856f19ada22ec31f33507ca037d4e455b38b52e
Sam Bugs <sambugs@protonmail.com>
2022-06-13 23:37:12 -0300

    feat: set denylisted

--- solidity/interfaces/adapters/IUniswapV3Adapter.sol
+++ solidity/interfaces/adapters/IUniswapV3Adapter.sol
@@ -26,6 +26,13 @@ interface IUniswapV3Adapter {
    */
   event CardinalityPerMinuteChanged(uint8 cardinalityPerMinute);
 
+  /**
+   * @notice Emitted when the denylist status is updated for some pools
+   * @param pools The pools that were updated
+   * @param denylisted Whether they will be denylisted or not
+   */
+  event DenylistChanged(address[] pools, bool[] denylisted);
+
   /// @notice Thrown when one of the parameters is the zero address
   error ZeroAddress();
 
@@ -34,6 +41,9 @@ interface IUniswapV3Adapter {
 
   /// @notice Thrown when trying to set an invalid cardinality
   error InvalidCardinalityPerMinute();
+
+  /// @notice Thrown when trying to set a denylist but the given parameters are invalid
+  error InvalidDenylistParams();
 
   /**
    * @notice Returns the address of the Uniswap oracle
@@ -69,8 +79,16 @@ interface IUniswapV3Adapter {
   function cardinalityPerMinute() external view returns (uint8);
 
   /**
+   * @notice Returns whether the given pool is denylisted or not
+   * @param pool The pool to check
+   * @return Whether the given pool is denylisted or not
+   */
+  function isPoolDenylisted(address pool) external view returns (bool);
+
+  /**
    * @notice Sets the period to be used for the TWAP calculation
-   * @dev Will revert it is lower than the minimum period or greater than maximum period
+   * @dev Will revert it is lower than the minimum period or greater than maximum period.
+   *      Can only be called by users with the admin role
    *      WARNING: increasing the period could cause big problems, because Uniswap V3 pools might not support a TWAP so old
    * @param newPeriod The new period
    */
@@ -78,8 +96,19 @@ interface IUniswapV3Adapter {
 
   /**
    * @notice Sets the cardinality per minute to be used when increasing observation cardinality at the moment of adding support for pairs
-   * @dev WARNING: increasing the cardinality per minute will make adding support to a pair significantly costly
+   * @dev Will revert if the given cardinality is zero
+   *      Can only be called by users with the admin role
+   *      WARNING: increasing the cardinality per minute will make adding support to a pair significantly costly
    * @param cardinalityPerMinute The new cardinality per minute
    */
   function setCardinalityPerMinute(uint8 cardinalityPerMinute) external;
+
+  /**
+   * @notice Sets the denylist status for a set of pools
+   * @dev Will revert if amount of pools does not match the amount of bools
+   *      Can only be called by users with the admin role
+   * @param pools The pools that were updated
+   * @param denylisted Whether they will be denylisted or not
+   */
+  function setDenylisted(address[] calldata pools, bool[] calldata denylisted) external;
 }
