4183b277348d7819a69ee32eaac116aee98c2c08
Sam Bugs <101145325+0xsambugs@users.noreply.github.com>
2022-06-15 20:53:57 -0300

    feat: add set period (#15)

--- solidity/contracts/adapters/UniswapV3Adapter.sol
+++ solidity/contracts/adapters/UniswapV3Adapter.sol
@@ -10,19 +10,37 @@ contract UniswapV3Adapter is AccessControl, IUniswapV3Adapter {
 
   /// @inheritdoc IUniswapV3Adapter
   IStaticOracle public immutable UNISWAP_V3_ORACLE;
+  /// @inheritdoc IUniswapV3Adapter
+  uint16 public immutable MAX_PERIOD;
+  /// @inheritdoc IUniswapV3Adapter
+  uint16 public immutable MIN_PERIOD;
+  /// @inheritdoc IUniswapV3Adapter
+  uint16 public period;
 
-  // uint16 public immutable maxPeriod;
-  // uint16 public immutable minPeriod;
-  // uint16 public period;
   // uint8 public cardinalityPerMinute;
 
   constructor(InitialConfig memory _initialConfig) {
-    UNISWAP_V3_ORACLE = _initialConfig.uniswapV3Oracle;
     if (_initialConfig.superAdmin == address(0)) revert ZeroAddress();
+    UNISWAP_V3_ORACLE = _initialConfig.uniswapV3Oracle;
+    MAX_PERIOD = _initialConfig.maxPeriod;
+    MIN_PERIOD = _initialConfig.minPeriod;
     _setupRole(SUPER_ADMIN_ROLE, _initialConfig.superAdmin);
     _setRoleAdmin(ADMIN_ROLE, SUPER_ADMIN_ROLE);
     for (uint256 i; i < _initialConfig.initialAdmins.length; i++) {
       _setupRole(ADMIN_ROLE, _initialConfig.initialAdmins[i]);
     }
+
+    // Set the period
+    if (_initialConfig.initialPeriod < MIN_PERIOD || _initialConfig.initialPeriod > MAX_PERIOD)
+      revert InvalidPeriod(_initialConfig.initialPeriod);
+    period = _initialConfig.initialPeriod;
+    emit PeriodChanged(_initialConfig.initialPeriod);
+  }
+
+  /// @inheritdoc IUniswapV3Adapter
+  function setPeriod(uint16 _newPeriod) external onlyRole(ADMIN_ROLE) {
+    if (_newPeriod < MIN_PERIOD || _newPeriod > MAX_PERIOD) revert InvalidPeriod(_newPeriod);
+    period = _newPeriod;
+    emit PeriodChanged(_newPeriod);
   }
 }
--- solidity/interfaces/adapters/IUniswapV3Adapter.sol
+++ solidity/interfaces/adapters/IUniswapV3Adapter.sol
@@ -7,15 +7,24 @@ interface IUniswapV3Adapter {
   /// @notice The initial adapter's configuration
   struct InitialConfig {
     IStaticOracle uniswapV3Oracle;
-    // uint16 maxPeriod;
-    // uint16 minPeriod;
-    // uint16 initialPeriod;
+    uint16 maxPeriod;
+    uint16 minPeriod;
+    uint16 initialPeriod;
     address superAdmin;
     address[] initialAdmins;
   }
 
+  /**
+   * @notice Emitted when a new period is set
+   * @param period The new period
+   */
+  event PeriodChanged(uint32 period);
+
   /// @notice Thrown when one of the parameters is the zero address
   error ZeroAddress();
+
+  /// @notice Thrown when trying to set an invalid period
+  error InvalidPeriod(uint16 period);
 
   /**
    * @notice Returns the address of the Uniswap oracle
@@ -23,4 +32,32 @@ interface IUniswapV3Adapter {
    * @return The address of the Uniswap oracle
    */
   function UNISWAP_V3_ORACLE() external view returns (IStaticOracle);
+
+  /**
+   * @notice Returns the maximum possible period
+   * @dev Cannot be modified
+   * @return The maximum possible period
+   */
+  function MAX_PERIOD() external view returns (uint16);
+
+  /**
+   * @notice Returns the minimum possible period
+   * @dev Cannot be modified
+   * @return The minimum possible period
+   */
+  function MIN_PERIOD() external view returns (uint16);
+
+  /**
+   * @notice Returns the period used for the TWAP calculation
+   * @return The period used for the TWAP
+   */
+  function period() external view returns (uint16);
+
+  /**
+   * @notice Sets the period to be used for the TWAP calculation
+   * @dev Will revert it is lower than the minimum period or greater than maximum period
+   *      WARNING: increasing the period could cause big problems, because Uniswap V3 pools might not support a TWAP so old
+   * @param newPeriod The new period
+   */
+  function setPeriod(uint16 newPeriod) external;
 }
