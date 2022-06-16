140ff075ee3010ba0bb6897532fb57a345fa4bcb
Sam Bugs <sambugs@protonmail.com>
2022-06-16 14:01:35 -0300

    refactor: set denylist for pairs

--- solidity/interfaces/adapters/IUniswapV3Adapter.sol
+++ solidity/interfaces/adapters/IUniswapV3Adapter.sol
@@ -15,6 +15,12 @@ interface IUniswapV3Adapter is IPriceOracle {
     address[] initialAdmins;
   }
 
+  /// @notice A pair of tokens
+  struct Pair {
+    address tokenA;
+    address tokenB;
+  }
+
   /**
    * @notice Emitted when a new period is set
    * @param period The new period
@@ -28,11 +34,11 @@ interface IUniswapV3Adapter is IPriceOracle {
   event CardinalityPerMinuteChanged(uint8 cardinalityPerMinute);
 
   /**
-   * @notice Emitted when the denylist status is updated for some pools
-   * @param pools The pools that were updated
+   * @notice Emitted when the denylist status is updated for some pairs
+   * @param pairs The pairs that were updated
    * @param denylisted Whether they will be denylisted or not
    */
-  event DenylistChanged(address[] pools, bool[] denylisted);
+  event DenylistChanged(Pair[] pairs, bool[] denylisted);
 
   /**
    * @notice Emitted when support is updated (added or modified) for a new pair
@@ -91,11 +97,12 @@ interface IUniswapV3Adapter is IPriceOracle {
   function cardinalityPerMinute() external view returns (uint8);
 
   /**
-   * @notice Returns whether the given pool is denylisted or not
-   * @param pool The pool to check
-   * @return Whether the given pool is denylisted or not
+   * @notice Returns whether the given pair is denylisted or not
+   * @param tokenA One of the pair's tokens
+   * @param tokenB The other of the pair's tokens
+   * @return Whether the given pair is denylisted or not
    */
-  function isPoolDenylisted(address pool) external view returns (bool);
+  function isPairDenylisted(address tokenA, address tokenB) external view returns (bool);
 
   /**
    * @notice When a pair is added to the oracle adapter, we will prepare all pools for the pair. Now, it could
@@ -127,11 +134,11 @@ interface IUniswapV3Adapter is IPriceOracle {
   function setCardinalityPerMinute(uint8 cardinalityPerMinute) external;
 
   /**
-   * @notice Sets the denylist status for a set of pools
-   * @dev Will revert if amount of pools does not match the amount of bools
+   * @notice Sets the denylist status for a set of pairs
+   * @dev Will revert if amount of pairs does not match the amount of bools
    *      Can only be called by users with the admin role
-   * @param pools The pools that were updated
+   * @param pairs The pairs to update
    * @param denylisted Whether they will be denylisted or not
    */
-  function setDenylisted(address[] calldata pools, bool[] calldata denylisted) external;
+  function setDenylisted(Pair[] calldata pairs, bool[] calldata denylisted) external;
 }
