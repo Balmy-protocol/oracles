d27023fccaedda83726c904d5be984a4182f4fbb
Sam Bugs <sambugs@protonmail.com>
2022-06-27 11:36:12 -0300

    chore: remove default admin for hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -76,10 +76,11 @@ const config: HardhatUserConfig = {
       default: 4,
     },
     admin: {
-      optimism: '0x308810881807189cAe91950888b2cB73A1CC5920', // Optimism
-      polygon: '0xCe9F6991b48970d6c9Ef99Fffb112359584488e3', // Polygon
-      arbitrum: '0x84F4836e8022765Af9FBCE3Bb2887fD826c668f1', // Arbitrum
-      default: '0x1a00e1e311009e56e3b0b9ed6f86f5ce128a1c01',
+      optimism: '0x308810881807189cAe91950888b2cB73A1CC5920',
+      polygon: '0xCe9F6991b48970d6c9Ef99Fffb112359584488e3',
+      arbitrum: '0x84F4836e8022765Af9FBCE3Bb2887fD826c668f1',
+      ethereum: '0xEC864BE26084ba3bbF3cAAcF8F6961A9263319C4',
+      hardhat: '0x1a00e1e311009e56e3b0b9ed6f86f5ce128a1c01',
     },
   },
   mocha: {
