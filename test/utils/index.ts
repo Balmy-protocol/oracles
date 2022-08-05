24f12ee3490257f421ed438721c0c184d36b95f9
Sam Bugs <sambugs@protonmail.com>
2022-08-05 14:53:19 -0300

    chore: delete erc20 mock

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,9 +1,8 @@
 import * as behaviours from './behaviours';
 import * as contracts from './contracts';
-import * as erc20 from './erc20';
 import * as evm from './evm';
 import * as bn from './bn';
 import * as wallet from './wallet';
 import * as erc165 from './erc165';
 
-export { contracts, behaviours, bn, erc20, evm, wallet, erc165 };
+export { contracts, behaviours, bn, evm, wallet, erc165 };
