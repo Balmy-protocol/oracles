de8f66d3b5467c15e3b106e4ce6c380f4d8caa90
Ged <0xged@protonmail.com>
2022-08-25 17:49:59 -0300

    fix: cleaning typechain script

--- tasks/npm-publish-clean-typechain.ts
+++ tasks/npm-publish-clean-typechain.ts
@@ -2,18 +2,21 @@ import { subtask } from 'hardhat/config';
 import { TASK_COMPILE_SOLIDITY_COMPILE_JOBS } from 'hardhat/builtin-tasks/task-names';
 import fs from 'fs/promises';
 
-subtask(TASK_COMPILE_SOLIDITY_COMPILE_JOBS, 'Clean mocks from types if needed').setAction(async (taskArgs, { run }, runSuper) => {
+subtask(TASK_COMPILE_SOLIDITY_COMPILE_JOBS, 'Clean tests from types if needed').setAction(async (taskArgs, { run }, runSuper) => {
   const compileSolOutput = await runSuper(taskArgs);
   if (!!process.env.PUBLISHING_NPM) {
-    console.log('🧹 Excluding mock types from typechain');
+    console.log('🫠 Removing all test references from typechain');
+    // Cleaning typechained/index
+    console.log(`  🧹 Excluding from main index`);
     const typechainIndexBuffer = await fs.readFile('./typechained/index.ts');
     const finalTypechainIndex = typechainIndexBuffer
       .toString('utf-8')
       .split(/\r?\n/)
-      .filter((line) => !line.includes('Mock'))
+      .filter((line) => !line.includes('test'))
       .join('\n');
     await fs.writeFile('./typechained/index.ts', finalTypechainIndex, 'utf-8');
-
+    // Cleaning typechained/solidity/contracts/index
+    console.log(`  🧹 Excluding from contracts index`);
     const typechainContractsIndex = await fs.readFile('./typechained/solidity/contracts/index.ts');
     const finalTypechainContractsIndex = typechainContractsIndex
       .toString('utf-8')
@@ -21,7 +24,8 @@ subtask(TASK_COMPILE_SOLIDITY_COMPILE_JOBS, 'Clean mocks from types if needed').
       .filter((line) => !line.includes('test'))
       .join('\n');
     await fs.writeFile('./typechained/solidity/contracts/index.ts', finalTypechainContractsIndex, 'utf-8');
-
+    // Cleaning typechained/factories/contracts/index
+    console.log(`  🧹 Excluding from factories contract's index`);
     const typechainFactoriesIndexBuffer = await fs.readFile('./typechained/factories/solidity/contracts/index.ts');
     const finalTypechainFactoriesIndex = typechainFactoriesIndexBuffer
       .toString('utf-8')
