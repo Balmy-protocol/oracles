57839a0fa18949636a602a10558b6991b8513811
Ged <0xged@protonmail.com>
2023-03-08 11:06:12 -0300

    test: adds integration for wsteth transformer

--- test/integration/transformer-oracle.spec.ts
+++ test/integration/transformer-oracle.spec.ts
@@ -5,71 +5,103 @@ import { BigNumber, BytesLike, constants, utils } from 'ethers';
 import { DeterministicFactory, DeterministicFactory__factory } from '@mean-finance/deterministic-factory';
 import { address as DETERMINISTIC_FACTORY_ADDRESS } from '@mean-finance/deterministic-factory/deployments/ethereum/DeterministicFactory.json';
 import { ProtocolTokenWrapperTransformer, TransformerRegistry } from '@mean-finance/transformers';
+import { JsonRpcSigner } from '@ethersproject/providers';
 import { expect } from 'chai';
 import { given, then, when } from '@utils/bdd';
 import { snapshot } from '@utils/evm';
 import { convertPriceToBigNumberWithDecimals, getTokenData } from '@utils/defillama';
 
-const BLOCK_NUMBER = 15583285;
+const BLOCK_NUMBER = 16783912;
 const EMPTY_BYTES: BytesLike = [];
 
 const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
 const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+const STETH = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84';
+const WSTETH = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0';
 
 describe('TransformerOracle', () => {
   let oracle: TransformerOracle;
+  let transformerRegistry: TransformerRegistry;
+  let admin: JsonRpcSigner;
   let snapshotId: string;
 
   before(async () => {
     await fork({ chain: 'ethereum', blockNumber: BLOCK_NUMBER });
     const { msig } = await getNamedAccounts();
-    const admin = await wallet.impersonate(msig);
+    admin = await wallet.impersonate(msig);
     await wallet.setBalance({ account: msig, balance: utils.parseEther('10') });
-    await deployments.run(['ChainlinkFeedRegistry', 'TransformerRegistry', 'ProtocolTokenWrapperTransformer', 'TransformerOracle'], {
-      resetMemory: true,
-      deletePreviousDeployments: false,
-      writeDeploymentsToFiles: false,
-    });
-    oracle = await ethers.getContract<TransformerOracle>('TransformerOracle');
-    const protocolTokenTransformer = await ethers.getContract<ProtocolTokenWrapperTransformer>('ProtocolTokenWrapperTransformer');
-    const transformerRegistry = await ethers.getContract<TransformerRegistry>('TransformerRegistry');
-
-    // Register WETH to protocol token transformer
-    await transformerRegistry.connect(admin).registerTransformers([
+    await deployments.run(
+      ['ChainlinkFeedRegistry', 'TransformerRegistry', 'ProtocolTokenWrapperTransformer', 'wstETHTransformer', 'TransformerOracle'],
       {
-        transformer: protocolTokenTransformer.address,
-        dependents: [WETH],
-      },
-    ]);
+        resetMemory: true,
+        deletePreviousDeployments: false,
+        writeDeploymentsToFiles: false,
+      }
+    );
+    oracle = await ethers.getContract<TransformerOracle>('TransformerOracle');
+    transformerRegistry = await ethers.getContract<TransformerRegistry>('TransformerRegistry');
     snapshotId = await snapshot.take();
   });
 
   beforeEach(async () => {
     await snapshot.revert(snapshotId);
   });
-
-  /* 
-   This test is meant to use a lot of different components. The idea is that the quote for WETH => DAI will:
-   1. Transform WETH to ETH in the Transformer oracle
-   2. Delegate the quote from the aggregator to the Chainlink oracle
-   3. Use the Chainlink registry in the Chainlink Oracle
-   */
-  when('using the transformer oracle', async () => {
-    given(async () => {
-      await oracle.addSupportForPairIfNeeded(WETH, DAI, EMPTY_BYTES);
+  describe('using the transformer oracle', async () => {
+    when('using the wstETH transformer', () => {
+      given(async () => {
+        const wstETHTransformer = await ethers.getContract<ProtocolTokenWrapperTransformer>('wstETHTransformer');
+        // Register WSTETH to wstETH transformer
+        await transformerRegistry.connect(admin).registerTransformers([
+          {
+            transformer: wstETHTransformer.address,
+            dependents: [WSTETH],
+          },
+        ]);
+        await oracle.addSupportForPairIfNeeded(WSTETH, STETH, EMPTY_BYTES);
+        await oracle.addSupportForPairIfNeeded(STETH, DAI, EMPTY_BYTES);
+      });
+      then('the pair is transformed correctly', async () => {
+        const wstETHToDaiQuote = await oracle.quote(WSTETH, utils.parseEther('1'), DAI, EMPTY_BYTES);
+        validateQuote(wstETHToDaiQuote, WSTETH, DAI);
+        const wstETHToSTETHQuote = await oracle.quote(WSTETH, utils.parseEther('1'), STETH, EMPTY_BYTES);
+        validateQuote(wstETHToSTETHQuote, WSTETH, STETH, 0.05);
+        const stETHToWSTETH = await oracle.quote(STETH, utils.parseEther('1'), WSTETH, EMPTY_BYTES);
+        validateQuote(stETHToWSTETH, WSTETH, STETH, 0.05);
+      });
     });
-    then('the pair is transformed correctly', async () => {
-      const quote = await oracle.quote(WETH, utils.parseEther('1'), DAI, EMPTY_BYTES);
-      validateQuote(quote);
+
+    /* 
+     This test is meant to use a lot of different components. The idea is that the quote for WETH => DAI will:
+     1. Transform WETH to ETH in the Transformer oracle
+     2. Delegate the quote from the aggregator to the Chainlink oracle
+     3. Use the Chainlink registry in the Chainlink Oracle
+     */
+    when('using the protocol token transformer wrapper transformer', () => {
+      given(async () => {
+        const protocolTokenTransformer = await ethers.getContract<ProtocolTokenWrapperTransformer>('ProtocolTokenWrapperTransformer');
+        // Register WETH to protocol token transformer
+        await transformerRegistry.connect(admin).registerTransformers([
+          {
+            transformer: protocolTokenTransformer.address,
+            dependents: [WETH],
+          },
+        ]);
+        await oracle.addSupportForPairIfNeeded(WETH, DAI, EMPTY_BYTES);
+      });
+      then('the pair is transformed correctly', async () => {
+        const quote = await oracle.quote(WETH, utils.parseEther('1'), DAI, EMPTY_BYTES);
+        validateQuote(quote, WETH, DAI);
+      });
     });
   });
-  async function validateQuote(quote: BigNumber) {
+
+  async function validateQuote(quote: BigNumber, tokenIn: string, tokenOut: string, thresholdPercentage?: number) {
     const { timestamp } = await ethers.provider.getBlock(BLOCK_NUMBER);
-    const tokenInData = await getTokenData('ethereum', WETH, timestamp);
-    const tokenOutData = await getTokenData('ethereum', DAI, timestamp);
+    const tokenInData = await getTokenData('ethereum', tokenIn, timestamp);
+    const tokenOutData = await getTokenData('ethereum', tokenOut, timestamp);
     const expectedAmountOut = convertPriceToBigNumberWithDecimals(tokenInData.price / tokenOutData.price, tokenOutData.decimals);
 
-    const TRESHOLD_PERCENTAGE = 2; // 2% price diff tolerance
+    const TRESHOLD_PERCENTAGE = thresholdPercentage ?? 2; // 2% price diff tolerance
 
     const threshold = expectedAmountOut.mul(TRESHOLD_PERCENTAGE * 10).div(100 * 10);
     const [upperThreshold, lowerThreshold] = [expectedAmountOut.add(threshold), expectedAmountOut.sub(threshold)];
